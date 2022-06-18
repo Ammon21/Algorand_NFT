6cdc3d92dc0333e8afe099e93f7de83dad2abfa3
Ammon21 <Ammonb6@gmail.com>
2022-06-18 18:42:49 +0300

    Updates

---
+++ precache-manifest.42633a1989fe886d9bef82a83aad89ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f58b60f838a8825dafa3",
+    "url": "/Algorand-NFT/Client/react/static/css/main.90b9e7e0.chunk.css"
+  },
+  {
+    "revision": "f58b60f838a8825dafa3",
+    "url": "/Algorand-NFT/Client/react/static/js/main.f58b60f8.chunk.js"
+  },
+  {
+    "revision": "ccfaed377a9312a06ca7",
+    "url": "/Algorand-NFT/Client/react/static/js/1.ccfaed37.chunk.js"
+  },
+  {
+    "revision": "18369a20acac539a62d7",
+    "url": "/Algorand-NFT/Client/react/static/js/runtime~main.18369a20.js"
+  },
+  {
+    "revision": "a6e42a941f438de48acd5ed6fc9f023b",
+    "url": "/Algorand-NFT/Client/react/index.html"
+  }
+];
